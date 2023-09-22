a06456e214f74b1df0bea7e01eebbf15a6c98896
Fábio Leonel <92651510+fabioleonel09@users.noreply.github.com>
2023-09-21 23:27:38 -0300

    geralInterface: Projeto | arquivos iniciais.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace geralInterfaceWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnObterProdutos = new System.Windows.Forms.Button();
+            this.btnProdutosPorId = new System.Windows.Forms.Button();
+            this.btnIncluirProduto = new System.Windows.Forms.Button();
+            this.btnAtualizaProduto = new System.Windows.Forms.Button();
+            this.btnDeletarProduto = new System.Windows.Forms.Button();
+            this.lblLista = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(12, 51);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(865, 278);
+            this.dgvDados.TabIndex = 0;
+            // 
+            // btnObterProdutos
+            // 
+            this.btnObterProdutos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnObterProdutos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnObterProdutos.Location = new System.Drawing.Point(117, 335);
+            this.btnObterProdutos.Name = "btnObterProdutos";
+            this.btnObterProdutos.Size = new System.Drawing.Size(124, 51);
+            this.btnObterProdutos.TabIndex = 1;
+            this.btnObterProdutos.Text = "Verificar";
+            this.btnObterProdutos.UseVisualStyleBackColor = true;
+            this.btnObterProdutos.Click += new System.EventHandler(this.btnObterProdutos_Click);
+            // 
+            // btnProdutosPorId
+            // 
+            this.btnProdutosPorId.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProdutosPorId.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProdutosPorId.Location = new System.Drawing.Point(247, 335);
+            this.btnProdutosPorId.Name = "btnProdutosPorId";
+            this.btnProdutosPorId.Size = new System.Drawing.Size(124, 51);
+            this.btnProdutosPorId.TabIndex = 2;
+            this.btnProdutosPorId.Text = "Verificar por identificador";
+            this.btnProdutosPorId.UseVisualStyleBackColor = true;
+            this.btnProdutosPorId.Click += new System.EventHandler(this.btnProdutosPorId_Click);
+            // 
+            // btnIncluirProduto
+            // 
+            this.btnIncluirProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIncluirProduto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIncluirProduto.Location = new System.Drawing.Point(377, 335);
+            this.btnIncluirProduto.Name = "btnIncluirProduto";
+            this.btnIncluirProduto.Size = new System.Drawing.Size(124, 51);
+            this.btnIncluirProduto.TabIndex = 3;
+            this.btnIncluirProduto.Text = "Incluir";
+            this.btnIncluirProduto.UseVisualStyleBackColor = true;
+            this.btnIncluirProduto.Click += new System.EventHandler(this.btnIncluirProduto_Click);
+            // 
+            // btnAtualizaProduto
+            // 
+            this.btnAtualizaProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAtualizaProduto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtualizaProduto.Location = new System.Drawing.Point(507, 335);
+            this.btnAtualizaProduto.Name = "btnAtualizaProduto";
+            this.btnAtualizaProduto.Size = new System.Drawing.Size(124, 51);
+            this.btnAtualizaProduto.TabIndex = 4;
+            this.btnAtualizaProduto.Text = "Atualizar";
+            this.btnAtualizaProduto.UseVisualStyleBackColor = true;
+            this.btnAtualizaProduto.Click += new System.EventHandler(this.btnAtualizaProduto_Click);
+            // 
+            // btnDeletarProduto
+            // 
+            this.btnDeletarProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDeletarProduto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeletarProduto.Location = new System.Drawing.Point(637, 335);
+            this.btnDeletarProduto.Name = "btnDeletarProduto";
+            this.btnDeletarProduto.Size = new System.Drawing.Size(124, 51);
+            this.btnDeletarProduto.TabIndex = 5;
+            this.btnDeletarProduto.Text = "Apagar";
+            this.btnDeletarProduto.UseVisualStyleBackColor = true;
+            this.btnDeletarProduto.Click += new System.EventHandler(this.btnDeletarProduto_Click);
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLista.Location = new System.Drawing.Point(369, 18);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(144, 19);
+            this.lblLista.TabIndex = 6;
+            this.lblLista.Text = "Lista de Produtos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 429);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.btnDeletarProduto);
+            this.Controls.Add(this.btnAtualizaProduto);
+            this.Controls.Add(this.btnIncluirProduto);
+            this.Controls.Add(this.btnProdutosPorId);
+            this.Controls.Add(this.btnObterProdutos);
+            this.Controls.Add(this.dgvDados);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Integração - WinForms";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnObterProdutos;
+        private System.Windows.Forms.Button btnProdutosPorId;
+        private System.Windows.Forms.Button btnIncluirProduto;
+        private System.Windows.Forms.Button btnAtualizaProduto;
+        private System.Windows.Forms.Button btnDeletarProduto;
+        private System.Windows.Forms.Label lblLista;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Microsoft.VisualBasic;
+
+namespace geralInterfaceWinForm
+{
+    public partial class Form1 : Form
+    {
+        #region VARIAVEIS E CONSTANTES
+        string apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+        string URI = "";
+        int codigoProduto = 1;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObterProdutos_Click(object sender, EventArgs e)
+        {
+            GetAllProdutos();
+        }
+
+        private void btnProdutosPorId_Click(object sender, EventArgs e)
+        {
+            InputBox();
+            if (codigoProduto != -1)
+            {
+                GetProdutoById(codigoProduto);
+            }
+        }
+
+        private void btnIncluirProduto_Click(object sender, EventArgs e)
+        {
+            AddProduto();
+        }
+
+        private void btnAtualizaProduto_Click(object sender, EventArgs e)
+        {
+            InputBox();
+            if (codigoProduto != -1)
+            {
+                UpdateProduto(codigoProduto);
+            }
+        }
+
+        private void btnDeletarProduto_Click(object sender, EventArgs e)
+        {
+            InputBox();
+            if (codigoProduto != -1)
+            {
+                DeleteProduto(codigoProduto);
+            }
+        }
+
+        private async void GetAllProdutos()
+        {
+            URI = apiUrl;
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(URI))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var ProdutoJsonString = await response.Content.ReadAsStringAsync();
+                        dgvDados.DataSource = JsonConvert.DeserializeObject<Produto[]>(ProdutoJsonString).ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não foi possível obter o produto : " + response.StatusCode);
+                    }
+                }
+            }
+        }
+
+        private async void GetProdutoById(int codProduto)
+        {
+            using (var client = new HttpClient())
+            {
+                BindingSource bsDados = new BindingSource();
+                URI = apiUrl + "/" + codProduto.ToString();
+                HttpResponseMessage response = await client.GetAsync(URI);
+                if (response.IsSuccessStatusCode)
+                {
+                    var ProdutoJsonString = await response.Content.ReadAsStringAsync();
+                    bsDados.DataSource = JsonConvert.DeserializeObject<Produto>(ProdutoJsonString);
+                    dgvDados.DataSource = bsDados;
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao obter o produto : " + response.StatusCode);
+                }
+            }
+        }
+
+        private async void AddProduto()
+        {
+            URI = apiUrl;
+            Produto prod = new Produto();
+            prod.Nome = "NoteBook Lenovo";
+            prod.Categoria = "Notebooks";
+            prod.Preco = 1200.00M;
+            using (var client = new HttpClient())
+            {
+                var serializedProduto = JsonConvert.SerializeObject(prod);
+                var content = new StringContent(serializedProduto, Encoding.UTF8, "application/json");
+                var result = await client.PostAsync(URI, content);
+            }
+            GetAllProdutos();
+        }
+
+        private async void UpdateProduto(int codProduto)
+        {
+            URI = apiUrl;
+            Produto prod = new Produto();
+            prod.Id = codProduto;
+            prod.Nome = "NoteBook Apple";
+            prod.Categoria = "Notebooks";
+            prod.Preco = 9900.00M; // atualizando o preço do produto
+
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await client.PutAsJsonAsync(URI + "/" + prod.Id, prod);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Produto atualizado");
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao atualizar o produto : " + responseMessage.StatusCode);
+                }
+            }
+            GetAllProdutos();
+        }
+
+        private async void DeleteProduto(int codProduto)
+        {
+            URI = apiUrl;
+            int ProdutoID = codProduto;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(URI);
+                HttpResponseMessage responseMessage = await
+client.DeleteAsync(String.Format("{0}/{1}", URI, ProdutoID));
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Produto excluído com sucesso");
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao excluir o produto  : " + responseMessage.StatusCode);
+                }
+            }
+            GetAllProdutos();
+        }
+
+        private void InputBox()
+        {
+            /* usando a função VB.Net para exibir um prompt para o usuário informar a senha */
+            string Prompt = "Informe o código do Produto.";
+            string Titulo = "Sobre os Produtos:";
+            string Resultado = Microsoft.VisualBasic.Interaction.InputBox(Prompt, Titulo, "9", 600, 350);
+            /* verifica se o resultado é uma string vazia o que indica que foi cancelado. */
+            if (Resultado != "")
+            {
+                codigoProduto = Convert.ToInt32(Resultado);
+            }
+            else
+            {
+                codigoProduto = -1;
+            }
+        }
+    }
+}
